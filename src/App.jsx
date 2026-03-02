1c916b548653fd1928915533051a5b1149c4a0ab
Brandon Koeck <bkoeck3@gmail.com>
2026-03-01 19:26:12 -0600

    add draft of cards and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const placeData = [
+  {
+    "name": "Pure Joy Toys",
+    "desc": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
+    "id": 1
+  },
+  {
+    "name": "Otaku Cafe",
+    "desc": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
+    "id": 2
+  }
+]
 
+function PlaceCard(props) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className="place-container">
+    <div className="place-card">
+      <h3>{props.place.name}</h3>
+      <p>{props.place.desc}</p>
+    </div>
+  </div>
+  )
+}
+
+function App() {
+  return (
+    <div className = "app-container">
+      <h1>Cool spots in San Antonio</h1>
+      <div className = "cards-grid">
+        {placeData.map((place) => (
+          <PlaceCard key={place.id} place={place}/>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
